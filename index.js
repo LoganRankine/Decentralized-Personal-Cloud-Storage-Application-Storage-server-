263a0b36dbb1292e29c9b237333366d913be2fb6
LoganRankine <72498942+LoganRankine@users.noreply.github.com>
2023-02-03 16:32:42 +0000

    File server now recieves post request to make directory along with a username. A directory is then made using the new user, username

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
 let fs = require('fs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+//Read configure file before server start
+let server_configuration = fs.readFileSync('server_config.txt')
+
+let first_start = server_configuration.toString()
+first_start.split(':')[1]
 
 let requestsAllowed = false;
-
+app.post('/', async (req,res)=>{
+    //res.sendFile(__dirname + '/set_up.html')
+    var user1 = req.body.user
+    console.log(user1)
+    const user = {lastname: 'rankine'}
+    res.send(user)
+})
 //Create user directory
-app.get('/CreateUserDirectory', async (req,res)=>{
-    res.send('logan')
-    console.log('user directory creation request')
+app.post('/CreateUserDirectory', async (req,res)=>{
+     //Create directory where user files stored
+     let user = req.body.user;
+     let userDirectory = __dirname + "/UserFolders/" + user;
+    try {
+        if (!fs.existsSync(userDirectory)) {
+        fs.mkdirSync(userDirectory);
+        console.log("Created user folder:", user)
+        let response = {CreatedDirectory: userDirectory}
+        res.send(response)
+        }
+    } catch (err) {
+        console.error(err);
+        res.send(err)
+    }
 })
 
 //Sign into server and allow request
